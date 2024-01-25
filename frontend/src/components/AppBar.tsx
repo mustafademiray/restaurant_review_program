dd87dde8bd99dde2e57e166aa7ba6d13ac0f07e3
mustafademiray <mustafademiray@protonmail.com>
2024-01-26 01:36:14 +0300

    fixing the hydration issue

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -1,5 +1,27 @@
+// import { FC } from "react";
+// import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
+// export const AppBar: FC = () => {
+//   return (
+//     <div className="flex w-full justify-end">
+//       <WalletMultiButton />
+//     </div>
+//   );
+// };
+
 import { FC } from "react";
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import dynamic from "next/dynamic";
+
+// Dynamically import WalletMultiButton with SSR turned off
+const WalletMultiButton = dynamic(
+  () =>
+    import("@solana/wallet-adapter-react-ui").then(
+      (mod) => mod.WalletMultiButton
+    ),
+  {
+    ssr: false,
+  }
+);
 
 export const AppBar: FC = () => {
   return (
